771af8cc472d6f04daf63815de36f714a7e3ca75
Ar7373 <102576425+Ar7373@users.noreply.github.com>
2022-07-21 22:11:51 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,16 @@
+#spam bot by Ar73
+#first install pyautogui using "pip install pyautogui"
+import pyautogui as pi
+import time
+
+m = input('Enter the message: ')
+n = input('Enter the number of messages: ')
+
+time.sleep(5)
+#enter the number to be cooldowned here
+print('NOWW')
+#you can change 'NOWW' to anthing
+for i in range(0, (int(n))):
+    pi.typewrite(m + '\n')
+
+#easy spam bot by Ar73 :))
